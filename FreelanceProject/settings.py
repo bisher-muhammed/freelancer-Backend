559305b4ee2c1cc79548cb79c5f7fd1f05e90732
bisher-muhammed <110516326+bisher-muhammed@users.noreply.github.com>
2026-05-06 15:51:17 +0530

    cors updated

--- FreelanceProject/settings.py
+++ FreelanceProject/settings.py
@@ -50,7 +50,7 @@ CORS_ALLOWED_ORIGINS = [
 
 # allow credentials if you're using JWT or session auth
 CORS_ALLOW_CREDENTIALS = True
-
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Application definition
@@ -85,7 +85,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -183,6 +183,11 @@ REST_FRAMEWORK = {
 
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "https://freelancerhub.online",
+    "https://freelancer-frontend-blue.vercel.app",
+]
 
 
 
